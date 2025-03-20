5c09380d26bee2e0f217ac33358fcd42c4ad4802
Diego Santiago <diegoa.santiagoh@gmail.com>
2025-03-20 14:01:27 -0300

    Contains duplicate problem

--- PHP/containsDuplicate.php
+++ PHP/containsDuplicate.php
@@ -1,1 +1,24 @@
 <?php
+
+class Solution
+{
+    function containsDuplicate(array $nums): bool
+    {
+        // opcion 1 -> O(N2)
+        /*foreach (array_count_values($nums) as $value) {
+            if ($value > 1) return true;
+        }
+        return false;*/
+
+        // opcion 2
+        $mapper = [];
+        foreach ($nums as $num) {
+            if (isset($mapper[$num])) return true;
+            $mapper[$num] = 1;
+        }
+        return false;
+    }
+}
+
+$solution = new Solution();
+var_dump($solution->containsDuplicate([1,2,3,1]));
