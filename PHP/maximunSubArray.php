f55face441b3faa554506857a4691c15ecd8b432
Diego Santiago <diegoa.santiagoh@gmail.com>
2025-03-25 20:45:33 -0300

    Maximun SubArray problem

--- PHP/maximunSubArray.php
+++ PHP/maximunSubArray.php
@@ -1,1 +1,27 @@
 <?php
+
+class Solution {
+
+    function maxSubArray(array $nums) {
+        $largestSum = $nums[0];
+        $currentSum = 0;
+        foreach ($nums as $num) {
+            $currentSum += $num;
+            if ($num > $currentSum) {
+                $currentSum = $num;
+            }
+            if ($currentSum > $largestSum) {
+                $largestSum = $currentSum;
+            }
+        }
+        return $largestSum;
+    }
+}
+
+$solution = new Solution();
+$test1 = [-2,1,-3,4,-1,2,1,-5,4]; // 6
+$test2 = [1]; // 1
+$test3 = [5,4,-1,7,8]; // 23
+$test4 = [-2,1]; // 1
+$test5 = [-1,2,2,-3]; // 4
+echo $solution->maxSubArray($test5);
