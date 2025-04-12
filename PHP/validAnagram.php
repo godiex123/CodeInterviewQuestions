bd4289704486cc1a261d3807e21e5be4317718b1
Diego Santiago <diegoa.santiagoh@gmail.com>
2025-04-11 20:15:36 -0400

    valid anagrram problem

--- PHP/validAnagram.php
+++ PHP/validAnagram.php
@@ -1,1 +1,15 @@
 <?php
+
+class Solution
+{
+    public function isAnagram(string $s, string $t): bool
+    {
+        if (strlen($s) !== strlen($t)) return false;
+        return count_chars($s, 1) === count_chars($t, 1);
+    }
+}
+
+$solution = new Solution();
+$s = 'anagram';
+$t = '';
+var_dump($solution->isAnagram($s, $t));
