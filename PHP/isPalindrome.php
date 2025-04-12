7331199369e9aeb0f2e2b6d4159e71cee2c125c5
Diego Santiago <diegoa.santiagoh@gmail.com>
2025-04-11 20:15:04 -0400

    is palindrome problem

--- PHP/isPalindrome.php
+++ PHP/isPalindrome.php
@@ -1,1 +1,27 @@
 <?php
+
+class Solution
+{
+    public function isPalindrome(array $nums): bool
+    {
+        $j = count($nums) - 1;
+        foreach ($nums as $num) {
+            if ($num != $nums[$j]) return false;
+            $j--;
+        }
+        return true;
+    }
+}
+
+$solution = new Solution();
+var_dump(
+    $solution->isPalindrome([1,2,1])
+);
+
+var_dump(
+    $solution->isPalindrome([-1,2,1])
+);
+
+var_dump(
+    $solution->isPalindrome([10])
+);
